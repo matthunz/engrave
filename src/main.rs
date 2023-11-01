c0527309e7dc67e04ee4f05fead81e0216498144
Matt Hunzinger <matthunz2@gmail.com>
2023-11-01 07:00:20 -0400

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 use dioxus::prelude::*;
-
 use editor::Editor;
 use log::LevelFilter;
 
 fn main() {
-    dioxus_logger::init(LevelFilter::Info).expect("failed to init logger");
+    dioxus_logger::init(LevelFilter::Info).unwrap();
     console_error_panic_hook::set_once();
-
-    log::info!("starting app");
     dioxus_web::launch(app);
 }
 

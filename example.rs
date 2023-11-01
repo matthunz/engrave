9caf4aa10a8191b8f83e1e4fa1ed0a40111c46b4
Matt Hunzinger <matthunz2@gmail.com>
2023-11-01 06:28:00 -0400

    Refactor component

--- example.rs
+++ example.rs
@@ -1,9 +1,3 @@
-fn app(cx: Scope) -> Element {
-    let mut count = use_state(cx, || 0);
-
-    render! {
-        h1 { "High-Five counter: {count}" }
-        button { onclick: move |_| count += 1, "Up high!" }
-        button { onclick: move |_| count -= 1, "Down low!" }
-    }
+fn main() {
+    
 }
